9b07f5d9f5c98ed6359febbaccae3cc88bc37ae6
Luis Moncada <raketentrakt0r@gmail.com>
2022-02-17 15:31:03 +0100

    fileup not working

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
+//const post = requires("../models/post.model") to use the post routes and we need to define 
+//const {findById} = require("../models.post.model")    the variable to find by Id
+
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+                        //Post.find()
+  res.render("index"); // .then(res.render("index",{post}))
 });
 
 module.exports = router;
